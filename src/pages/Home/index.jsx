ae048ffe2a2f3b84ee4173f0e467b6b868aa1917
Alisson Matias <alissonmds00@gmail.com>
2022-08-11 19:09:03 -0300

    feat: added new items to the UI

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,8 @@
 import './styles.css'
+import Logo from '../../assets/logo.png'
+import Cesta from '../../assets/cesta.svg'
+import UserIcon from '../../assets/userIcon.svg'
+import Search from '../../assets/search.svg'
 export function Home() {
 
   return (
@@ -6,23 +10,19 @@ export function Home() {
       <header>
           <ul>
             <li>
-              <img src="" alt="" />
-            </li>
-            <li>
-              <form action="">
-                <input type="search" name="" id="searchbox" placeholder='O que você está buscando?' />
-                <button id='searchbutton'></button>
-              </form>
+              <a href="">
+                <img src={ Logo } id="logo" alt="" />
+              </a>
             </li>
             <li id='logar'>
-              <img src="" alt="" />
+              <img id='userIcon' src={ UserIcon } alt="" />
               <p>
                 Bem-vind@!
                 <a href="">Acesse aqui o seu perfil</a>
               </p>
             </li>
             <li>
-              <a href=""><img src="" alt="" /></a>
+              <a href=""><img src={Cesta} alt="" title='Cesta de compras'/></a>
             </li>
           </ul>
       </header>
@@ -31,7 +31,13 @@ export function Home() {
 
       </main>
       <footer>
-
+        <img src={Logo} alt="" />
+        <ul>
+          <strong>Acesse nosso projeto</strong>
+          <li><a href="https://github.com/alissonmds00" target="_blank">Alisson Matias</a></li>
+          <li><a href="https://github.com/guiramos1504" target="_blank">Guilherme Ramos</a></li>
+          <li><a href="https://github.com/GuiMazur" target="_blank">João Guilherme Mazur</a></li>
+        </ul>
       </footer>
     </div>
   )
