338d8478be55a7e5695f0cb1b3c96174cc594295
Alisson Matias <alissonmds00@gmail.com>
2022-08-13 16:03:18 -0300

    fix: substituindo alguns arquivos HTML e CSS

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,50 +1,47 @@
-import './styles.css'
+import '../../styles/global.css'
+import './styles.css' 
 import Logo from '../../assets/logo.svg'
 import Cesta from '../../assets/cesta.svg'
 import UserIcon from '../../assets/userIcon.svg'
 import Search from '../../assets/search.svg'
+
 export function Home() {
 
   return (
     <div className="container">
       <header>
-        <a href="#">
+        <a href="">
           <img src={Logo} id="logo" alt="" />
         </a>
-          <form action="" id='searchbox'>
-            <input type="search" name="" placeholder='O que você está buscando?' disabled/>
-            <button id='searchbutton'><img src={Search} alt="" /></button>
-          </form>
-        <div id='userarea'>
-        <ul id='userarea'>
-          <li id='logar'>
-            <img id='usericon' src={UserIcon} alt="" />
+
+        <section id='searchbox'>
+          <input disabled type="search" placeholder='O que você está buscando?'/>
+          <img src={Search} alt="" />
+        </section>
+
+        <section id='userArea'>
+          <div>
+            <img id='userIcon' src={UserIcon} alt="Foto do usuário" />
             <p>
               Bem-vind@!
               <a href="">Acesse aqui o seu perfil</a>
             </p>
-          </li>
-          <li id='buycart'>
-            <a href=""><img src={Cesta} alt="" title='Cesta de compras' /></a>
-          </li>
-        </ul>
-        </div>
+          </div>
+          <a href="">
+            <img src={Cesta} alt="" title='Cesta de compras'/>
+          </a>
+        </section>
+        
       </header>
 
       <main>
-
       </main>
+      
       <footer>
         <img src={Logo} alt="" />
-        <div id="contact">
-          <p>2022</p>
-          <ul>
-            <li><a href="">Trabalhe conosco</a></li>
-            <li><a href="">Termos e condições</a></li>
-            <li><a href="">Acessibilidade</a></li>
-            <li><a href="">Contato</a></li>
-          </ul>
-        </div>
+        <p>
+          Trabalhe conosco  | Termos e condições | Privacidade | Acessibilidade  | Contato
+        </p>
         <ul>
           <strong>Acesse nosso projeto</strong>
           <li><a href="https://github.com/alissonmds00" target="_blank">Alisson Matias</a></li>
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,13 +1,41 @@
 import './styles.css'
+import Logo from '../../assets/logo.svg'
 
 export function Login() {
     return (
         <div className='container'>
+            <header>
+                <img id='logo' src={Logo} alt="" />
+            </header>
             <main>
-
+                <div id='loginarea'>
+                    <div id='titulo'>
+                        <h1 className='h2'>Faça seu Login</h1>
+                        <p className='semibold18'>Entre com as suas informações de cadastro</p>
+                    </div>
+                    <form action="login" id='loginform'>
+                        <label htmlFor="emailbar">E-mail</label>
+                        <div className='login'>
+                            <input type="email" name="email" id="emailbar" className='loginitem' placeholder='Digite o seu email' autoFocus />
+                        </div>
+                        <label htmlFor="password">Senha</label>
+                        <div className='login'>
+                            <input type="password"
+                                name="senha"
+                                id="passwordbar"
+                                className='loginitem'
+                                placeholder='Digite a sua senha' />
+                        </div>
+                        <a href="">Esqueci minha senha</a>
+                        <button>Entrar</button>
+                    </form>
+                    <div>
+                        <p>Não possui uma conta? <a href=""> Cadastre-se</a></p>
+                    </div>
+                </div>
             </main>
             <footer>
-                
+
             </footer>
         </div>
     )
