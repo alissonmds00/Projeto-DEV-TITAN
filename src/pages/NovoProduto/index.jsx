59946a98daeedef31bb849ca162bb49403492817
Guilherme Mazur <jjguilh@gmail.com>
2022-09-09 19:37:26 -0300

    refactor: Grouping states in just one state Form

--- src/pages/NovoProduto/index.jsx
+++ src/pages/NovoProduto/index.jsx
@@ -1,4 +1,4 @@
-import styles from '../../components/ProdutoADM/styles.module.css'
+import styles from './styles.module.css'
 import global from '../../styles/global.module.css'
 import { Header } from '../../components/Header/Header'
 import { Footer } from '../../components/Footer/Footer'
@@ -10,11 +10,16 @@ import { useEffect } from 'react'
 import starting from '../../functions/starting'
 
 export const NovoProduto = () => {
-    const [produtoNome, setProdutoNome] = useState('')
-    const [produtoDesc, setProdutoDesc] = useState('')
-    const [produtoValor, setProdutoValor] = useState(0)
-    const [produtoQuantidade, setProdutoQuantidade] = useState(0)
-    const [produtoImagem, setProdutoImagem] = useState('')
+
+    const initForm = {
+        name:  '',
+        price:  0,
+        image: '',
+        description:  '',
+        stock:  0
+    }
+
+    const [form, setForm] = useState(initForm)
 
     useEffect(() => {
         starting()
@@ -22,32 +27,22 @@ export const NovoProduto = () => {
 
     function adicionarProduto(e) {
         e.preventDefault()
-        axios.post(`${import.meta.env.VITE_API}/products/register`, {
-            name:  produtoNome,
-            price:  produtoValor,
-            image: produtoImagem,
-            description:  produtoDesc,
-            stock:  produtoQuantidade
-        })
+        axios.post(`${import.meta.env.VITE_API}/products/register`, form)
         .then(r => 
             alert(r.data)
         )
         .then(() => {
-            setProdutoNome('')
-            setProdutoDesc('')
-            setProdutoValor(0)
-            setProdutoQuantidade(0)
-            setProdutoImagem('')
+            setForm(initForm)
         })
         .catch(e => 
             alert(e.response ? e.response.data : e)
         )
     }
     return (
-        <div className='container'>
+        <div className={styles.container}>
             <Header />
             <section className={styles.section}>
-                <Produto img={ produtoImagem } />
+                <Produto img={ form.image } />
                 <div>
                     <form onSubmit={adicionarProduto} name='areaForm'>
                         <div>
@@ -55,30 +50,30 @@ export const NovoProduto = () => {
                                 Nome do produto
                             </label>
                             <br />
-                            <input type="text" name='produtoNome' value={produtoNome} onChange={(e) => setProdutoNome(e.target.value)} />
+                            <input type="text" name='produtoNome' value={form.name} onChange={(e) => setForm({...form, name: e.target.value})} />
                             <br />
                             <label htmlFor="produtoIMG" className={global.semibold15}>
                                 Imagem do produto
                             </label>
                             <br />
-                            <input type="url" name='produtoIMG' value={produtoImagem} onChange={(e) => setProdutoImagem(e.target.value)} />
+                            <input type="url" name='produtoIMG' value={form.image} onChange={(e) => setForm({...form, image: e.target.value})} />
                             <br />
                             <label htmlFor="produtoDescricao" className={global.semibold15}>
                                 Descrição do Produto
                             </label>
                             <br />
-                            <textarea name="produtoDescricao" id="produtoDescricao" cols="30" rows="7" value={(produtoDesc)} onChange={(e) => setProdutoDesc(e.target.value)}></textarea>
+                            <textarea name="produtoDescricao" id="produtoDescricao" cols="30" rows="7" value={form.description} onChange={(e) => setForm({...form, description: e.target.value})}></textarea>
                         </div>
                         <div>
                             <label htmlFor="produtoQuant" className={global.semibold15}>
                                 Estoque
                             </label>
-                            <input type="number" name="produtoQuant" id="produtoQuant" value={produtoQuantidade} onChange={(e) => setProdutoQuantidade(parseFloat(e.target.value))} />
+                            <input type="number" name="produtoQuant" id="produtoQuant" value={form.stock} onChange={(e) => setForm({...form, stock: parseFloat(e.target.value)})} />
                             <br />
                             <label htmlFor="produtoPreco" className={global.semibold15} >
                                 Preço Unitário
                             </label>
-                            <input type="number" name="produtoPreco" id="produtoPreco" step='0.01' className={styles.inputNumber} value={produtoValor} onChange={(e) => setProdutoValor(parseFloat(e.target.value))} />
+                            <input type="number" name="produtoPreco" id="produtoPreco" step='0.01' className={styles.inputNumber} value={form.price} onChange={(e) => setForm({...form, price: parseFloat(e.target.value)})} />
                             <label htmlFor="">
                             </label>
                         </div>
